53adf29411d858c1e8d93b2d70cf6cc0c8883922
SylvainJunca <sylvain.junca@gmail.com>
2019-04-19 16:28:10 -0400

    setup the clairifai API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const particlesOptions = {
 	particles: {
@@ -20,6 +23,35 @@ const particlesOptions = {
 };
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input: ''
+		};
+	}
+
+	onInputChange = (event) => {
+		console.log(event.target.value);
+	};
+
+	onButtonSubmit = () => {
+		let app = new Clarifai.App({ apiKey: API_KEY });
+
+		app.models
+			.predict(
+				{ id: 'food', version: 'dfebc169854e429086aceb8368662641' },
+				'https://samples.clarifai.com/metro-north.jpg'
+			)
+			.then(
+				function(response) {
+					console.log(response);
+				},
+				function(err) {
+					// there was an error
+				}
+			);
+	};
+
 	render() {
 		return (
 			<div className="App">
@@ -27,7 +59,7 @@ class App extends Component {
 				<Navigation />
 				<Logo />
 				<Rank />
-				<ImageLinkForm />
+				<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
 				{/* <FaceRecognition /> */}
 			</div>
 		);
