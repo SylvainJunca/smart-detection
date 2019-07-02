8d6d4f197d1455d880bc86e932d5ff24feb7fabb
SylvainJunca <sylvain.junca@gmail.com>
2019-07-02 00:27:36 -0400

    Fixes image display

--- src/components/ImageRecognition/ImageRecognition.js
+++ src/components/ImageRecognition/ImageRecognition.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ImageRecognition = ({ imageURL }) => {
 	return (
 		<div className="center">
-			<img src={imageURL} alt="uploaded" />
+			<img style= {{maxWidth : '80%'}} src={imageURL} alt="uploaded" />
 		</div>
 	);
 };
