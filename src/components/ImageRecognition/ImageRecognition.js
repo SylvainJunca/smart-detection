5784016a24307651ee1f1b494ce6753e0da40b82
SylvainJunca <sylvain.junca@gmail.com>
2019-07-06 12:47:44 -0400

    fixes the image display in Safari

--- src/components/ImageRecognition/ImageRecognition.js
+++ src/components/ImageRecognition/ImageRecognition.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './ImageRecognition.css'
 
 const ImageRecognition = ({ imageURL }) => {
-	const image = imageURL ? <img src={imageURL} alt="uploaded" /> : '';
+	const image = imageURL ? <img id="imageResult" src={imageURL} alt="uploaded" /> : '';
 	return (
-		<span className="thumbnail center">
+		<span  className="thumbnail center">
 			{image}
 		</span>
 	);
